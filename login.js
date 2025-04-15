e5e5b05763e0283627b92302ff19d9490a7f490d
David22003 <149427618+David22003@users.noreply.github.com>
2025-04-16 01:33:46 +0530

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('loginForm').addEventListener('submit', function (e) {
-    e.preventDefault();
-  
-    // Simulate successful login
-    window.location.href = 'dashboard.html';
-  });
-  
